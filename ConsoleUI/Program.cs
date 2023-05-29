e09a16fe36e48e73839128f942470a3743373573
corenil <anil.aygun.37@outlook.com.tr>
2023-05-29 15:32:47 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+Console.WriteLine("-BASE-\n");
+
+ProductTest();
+Console.WriteLine("--\n\n--");
+//CategoryTest();
+
+Console.WriteLine("\n-PASSED-");
+
+
+static void ProductTest()
+{
+    ProductManager productManager = new ProductManager(new EfProductDal());
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + " : " + product.CategoryName);
+    }
+}
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName + " : " + category.CategoryId);
+    }
+}
+
+
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    // T tipini sadece IEntity interface'ini implemente eden entityler ile sınırlandırıldı.
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
